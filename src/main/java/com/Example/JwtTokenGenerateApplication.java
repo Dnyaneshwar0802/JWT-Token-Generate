f8da4ede058d8da43f9572ae2d9e9205a49cae8c
GroupProject-D <dnyaneshwarbagal0802@gmail.com>
2025-01-18 12:29:36 +0530

    Initial commit

---
+++ src/main/java/com/Example/JwtTokenGenerateApplication.java
@@ -0,0 +1,13 @@
+package com.Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenGenerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTokenGenerateApplication.class, args);
+	}
+
+}
